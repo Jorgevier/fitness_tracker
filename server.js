eccca0d51f18b24f99efcaf92d5888c722c61e53
Jorge Viera <Jorge5psgt@gmail.com>
2021-07-09 16:22:16 -0400

    deploying

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
-//     useNewUrlParser: true,
-//     useUnifiesTopology: true,
-//     useCreateIndex:true,
-//     useFindAndModify: false,
-// });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{
+    useNewUrlParser: true,
+    useUnifiesTopology: true,
+    useCreateIndex:true,
+    useFindAndModify: false,
+});
 
 
 
@@ -27,8 +27,8 @@ app.use(express.static(path.join(__dirname,"public")));
 app.use(require("./routes/api"))
 app.use(require("./routes/html_routes"))
 
-db.once('open', () => {
+// db.once('open', () => {
 app.listen(PORT, () =>{
     console.log(`App Listening pn port ${PORT}`);
 });
-});
+;
